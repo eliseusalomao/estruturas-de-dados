93e81cd5c61164dc2540ea748fcd823ce466f560
Eliseu Salomao <eliseusalomao20@gmail.com>
2023-05-02 17:01:59 -0300

    Comentando codigo

--- Atividade de Revisão para 1° prova/Lista com vetor/Pendrive.c
+++ Atividade de Revisão para 1° prova/Lista com vetor/Pendrive.c
@@ -1,9 +1,30 @@
+/***********************************************************
+* Nome: lista com vetores                                  *
+* Autor: Eliseu Salomão                                    *
+* Turma: 301B                                              *
+* Professor: Mayrton Dias                                  *
+* Disciplina: Estrutura de Dados                           *
+* Descrição: implementação de lista com vetor, com as      *
+*            operações de: criar lista,                    *   
+*                           excluir lista,                 *
+*                           inserir elemento,              *
+*                           inserir elemento no inicio,    *
+*                           inserir elemento por id,       *
+*                           remover elemento,              *
+*                           remover elemento por nome,     *
+*                           buscar elemento e              *
+*                           atualizar elemento.            *                                                   
+***********************************************************/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+// definição do tamanho para o vetor
 #define TAM 10
 
+// definição das estruturas que serão utilizadas
 typedef struct {
     char nomeArquivo[30];
     float tamanhoArquivo;
@@ -15,6 +36,7 @@ typedef struct {
     Pendrive *elementos;
 } Lista;
 
+// função de criar lista
 Lista* criarLista() {
     Lista *nova = (Lista*)malloc(sizeof(Lista));
     if(nova == NULL) {
@@ -32,24 +54,29 @@ Lista* criarLista() {
     return nova;
 }
 
+// função de excluir lista
 Lista* excluirLista(Lista *lista) {
     free(lista->elementos);
     free(lista);
     return NULL;
 }
 
+// função de imprimir lista
 void imprimirElementos(Lista *lista) {
     int i;
+    // verificando se a lista foi criada
     if(lista == NULL) {
         printf("A lista nao foi criada\n");
         return ;
     }
 
+    // verificando se o vetor possui algum elemento
     if(lista->id == 0) {
         printf("A lista esta vazia\n");
         return ;
     }
 
+    // o for irá percorrer todos os ids. Através da variável i podemos acessar a estrutura e o que está atribuído em determinada posição no vetor
     printf("Nome\t\tTamanho\t\tParticao\n");
     for(i = 0; i < lista->id; ++i) {
         printf("%d. %s\t",(i+1) , lista->elementos[i].nomeArquivo);
@@ -60,12 +87,16 @@ void imprimirElementos(Lista *lista) {
     printf("\n");
 }
 
+// função de inserir elemento
 int inserirElemento(Lista *lista, char *nome, float tamanho, char particao) {
+    // verificando se a lista está vazia
     if(lista == NULL) {
         printf("A lista nao foi criada");
         return 0;
     }
 
+    // inserindo elemento
+    // verificando se o vetor ainda possui espaço, se ouver, o próprio id irá servir para inserir um elemento em determinada posição
     if(lista->id<TAM) {
         strcpy(lista->elementos[lista->id].nomeArquivo, nome);
         lista->elementos[lista->id].tamanhoArquivo = tamanho;
@@ -78,38 +109,50 @@ int inserirElemento(Lista *lista, char *nome, float tamanho, char particao) {
     return 1;
 }
 
+// função de inserir elemento no inicio
 int inserirElementoInicio(Lista *lista, char *nome, float tamanho, char particao) {
     int i;
+    // verificando se a lista foi criada
     if (lista == NULL) {
         printf("A lista nao foi criada\n");
         return 0;
     }
 
+    // verificando se possui espaço no vetor para inserir um elemento.
     if (lista->id < TAM) {
+        // se houver iremos do ultimo elemento até o primeiro, passando todos os elementos de determinada posição, para posição + 1, deixando a posição 0 livre.
         for (i = lista->id; i > 0; --i) {
             strcpy(lista->elementos[i].nomeArquivo, lista->elementos[i-1].nomeArquivo);
             lista->elementos[i].tamanhoArquivo = lista->elementos[i-1].tamanhoArquivo;
             lista->elementos[i].particao = lista->elementos[i-1].particao;
         }
+        // inserindo elemento no inicio (posição 0)
         strcpy(lista->elementos[0].nomeArquivo, nome);
         lista->elementos[0].tamanhoArquivo = tamanho;
         lista->elementos[0].particao = particao;
         ++(lista->id);
+        // caso contrário, não há espaço
     } else {
         printf("Espaco esgotado");
         return 0;
     }
 }
 
+// função de inserir elemento por id
 int inserirElementoID(Lista *lista, char *nome, float tamanho, char particao, int posicao) {
     int i;
+
+    // verificando se a lista foi criada
     if (lista == NULL) {
         printf("A lista nao foi criada\n");
         return 0;
     }
 
+    // verificando se há espaço no vetor
     if (lista->id < TAM) {
+        // verificando se a posição passada está dentro do intervalo do tamanho do vetor
         if (posicao < lista->id) {
+            // iniciando o laço com a ultima posição do vetor, se 
             for (i = lista->id; i > posicao; --i) {
                 strcpy(lista->elementos[i].nomeArquivo, lista->elementos[i-1].nomeArquivo);
                 lista->elementos[i].tamanhoArquivo = lista->elementos[i-1].tamanhoArquivo;
@@ -149,9 +192,11 @@ int removerElemento(Lista *lista, int id) {
     return 0;
 }
 
+// função de remover elemento por nome
 int removerElementoNome(Lista *lista, char *nome) {
     int i, j;
 
+    // verificando se a lista está vazia
     if (lista == NULL){
         printf("A lista nao foi criada\n");
         return 0;
@@ -170,14 +215,17 @@ int removerElementoNome(Lista *lista, char *nome) {
     }
 }
 
+// função de buscar elemento
 int buscarElemento(Lista *lista, char *nome) {
     int i, j;
 
+    // verificando se a lista foi criada
     if (lista == NULL) {
         printf("A lista nao foi criada\n");
         return -1;
     }
 
+    // verificando se o nome passado corresponde ao nome de algum arquivo da lista comparando com o strcmp
     for (i = 0; i < lista->id; ++i) {
         if (strcmp(lista->elementos[i].nomeArquivo, nome) == 0) {
             return i;
@@ -186,6 +234,7 @@ int buscarElemento(Lista *lista, char *nome) {
     return -1;
 }
 
+// função de atualizar um elemento da lista, alterando seus valores
 int atualizarElemento(Lista *lista, char *busca, char *nome, float tamanho, char particao) {
     int i;
 
